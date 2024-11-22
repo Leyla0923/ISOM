46ea108e32e51fcdac508b398e7f6d5a7d3244fe
Leyla0923 <liky112369@gmail.com>
2024-11-22 14:44:25 +0800

    Update lab22_11.py

--- lab22_11.py
+++ lab22_11.py
@@ -2,13 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_page_config(page_title="Hello")
+#streamlit: h1-h3
+st.set_page_config(page_title="Hello") #it must be the first commend
 
 st.title("Welcome to ISOM3400 Evaluator")
-st.text("Objective: Discover whether you will fail the course")
+#st.text("Objective: Discover whether you will fail the course")
 st.write("Objective: Discover whether you will fail the course")
-
-
-
-#lab:
+st.markdown('---')
 
