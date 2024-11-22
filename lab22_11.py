d90d768e80bf8bc45cb0a97181fafedcb55da714
Leyla0923 <liky112369@gmail.com>
2024-11-22 14:41:20 +0800

    Update lab22_11.py

--- lab22_11.py
+++ lab22_11.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.set_page_config(page_title="Hello")
+
 st.title("Welcome to ISOM3400 Evaluator")
 st.text("Objective: Discover whether you will fail the course")
 st.write("Objective: Discover whether you will fail the course")
@@ -9,4 +11,4 @@ st.write("Objective: Discover whether you will fail the course")
 
 
 #lab:
-st.set_page_config(page_title="Hello")
+
