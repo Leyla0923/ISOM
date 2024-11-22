f7a23b202087cf476414ac9043fcc3579c783081
Leyla0923 <liky112369@gmail.com>
2024-11-22 14:49:47 +0800

    Update lab22_11.py

--- lab22_11.py
+++ lab22_11.py
@@ -11,8 +11,16 @@ st.write("Objective: Discover whether you will fail the course")
 st.markdown('---')
 st.header("Requirement Check for the course")
 st.subheader("Check List")
-st.write("""1. GroupExercise
+st.write("""1. Group Exercise
 2. Assignment
 3. Final Exam""")
+st.markdown('---')
 st.subheader("Requirement Check for the Assignment")
-
+st.markdown(
+"""
+The following list won't indent no matter what I try:
+- Item 1
+- Item 2
+- Item 3
+"""
+)
