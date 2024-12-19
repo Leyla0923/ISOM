0436c9eb2f0491da540cbc475406e0ad7e4d3500
Leyla0923 <liky112369@gmail.com>
2024-12-19 15:47:13 +0800

    Create wrtite.py

---
+++ wrtite.py
@@ -0,0 +1,4 @@
+st.text("Hello World")  # Plain text only
+st.write("**Hello World**")  # Shows bold text due to markdown
+st.write(pd.DataFrame())  # Can display dataframe
+st.write(plt.figure())  # Can display plots
